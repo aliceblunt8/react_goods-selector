aa1b203d3fa0ca315bd41e2571b97655b7eebaeb
Alisa <aliceblunt8@gmail.com>
2020-09-02 13:07:54 +0300

    Updates

---
+++ precache-manifest.df95f8c7076418642d2df4a828edf56f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592e3542820767f298288690072e8a9c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "473abd3c2abd6a39199f",
+    "url": "./static/css/main.4225da82.chunk.css"
+  },
+  {
+    "revision": "4dd0f18127de32a9553a",
+    "url": "./static/js/2.22b5d565.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.22b5d565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "473abd3c2abd6a39199f",
+    "url": "./static/js/main.c40026cf.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
